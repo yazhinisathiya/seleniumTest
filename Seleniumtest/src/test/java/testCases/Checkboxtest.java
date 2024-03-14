6496aceb7fff4d97bf0e979f6e0162609818efae
venkatesh <venkateshgiyengar@gmail.com>
2024-03-14 13:03:29 +0530

    Added Parametization in testNG.xml

--- Seleniumtest/src/test/java/testCases/Checkboxtest.java
+++ Seleniumtest/src/test/java/testCases/Checkboxtest.java
@@ -4,11 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import utilities.Screenshot;
@@ -18,9 +23,37 @@ public class Checkboxtest {
 	WebDriver driver;
 	WebElement chkbox;
 
-	@BeforeClass
-	public void setup() {
-		driver = new ChromeDriver();
+	@BeforeTest
+	@Parameters({"name","message"})
+	public void setup( String browser,@Optional("testing") String message) throws Exception {
+		if(browser.equalsIgnoreCase("ie"))
+		{
+		    //create IE instance
+			
+			driver = new InternetExplorerDriver();
+		}		
+		
+		//Verify if parameter passed from TestNG is Chrome
+		else if(browser.equalsIgnoreCase("chrome"))
+		{
+			//set path to chromedriver.exe
+			
+			//create Chrome instance
+			driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			//set path to chromedriver.exe
+			
+			//create FireFox instance
+			driver = new FirefoxDriver();
+		}
+		else
+		{
+			System.out.println(message);
+			//If no browser passed throw exception
+			throw new Exception("No browser specified");
+		}
 		driver.manage().window().maximize();
 	}
 	@Test
@@ -31,21 +64,20 @@ public class Checkboxtest {
 		Assert.assertEquals(title, expectedTitle);	
 		
 	}
-	@Test
-	public void testcase2() {
-		chkbox = driver.findElement(By.cssSelector("#isAgeSelected"));
-		chkbox.click();
-		//System.out.println("After Select : " + chkbox.isSelected());
-		Assert.assertTrue(chkbox.isSelected());		
-	}
-	@Test
-	public void testcase3() {
-		chkbox.click();
-		//System.out.println("After UnSelect : " + chkbox.isSelected());
-		
-		Assert.assertTrue(chkbox.isSelected());
-		
-	}
+
+	/*
+	 * @Test public void testcase2() { chkbox =
+	 * driver.findElement(By.cssSelector("#isAgeSelected")); chkbox.click();
+	 * //System.out.println("After Select : " + chkbox.isSelected());
+	 * Assert.assertTrue(chkbox.isSelected()); }
+	 * 
+	 * @Test public void testcase3() { chkbox.click();
+	 * //System.out.println("After UnSelect : " + chkbox.isSelected());
+	 * 
+	 * Assert.assertTrue(chkbox.isSelected());
+	 * 
+	 * }
+	 */
 	@AfterMethod
 	public void screenshottest() {
 		Screenshot.captureScreenshot(driver, "checkboxtesting");
