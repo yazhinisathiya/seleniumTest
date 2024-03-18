23a25e185735afb0da990f61a6e13b61b357c9eb
venkatesh <venkateshgiyengar@gmail.com>
2024-03-18 10:45:52 +0530

    Group Testcases

---
+++ Seleniumtest/src/test/java/testCases/TestNGGroupDemoTest.java
@@ -0,0 +1,31 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class TestNGGroupDemoTest {
+
+  @Test
+  public void Car1Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void Car2Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void Scooter1Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void Scooter2Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void Sedan1Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
