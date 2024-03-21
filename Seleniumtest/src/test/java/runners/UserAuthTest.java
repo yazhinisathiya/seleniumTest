73be6492355bfdc844a0520d6025da9e27145961
venkatesh <venkateshgiyengar@gmail.com>
2024-03-21 09:54:29 +0530

    Added Cucumber dependencies

---
+++ Seleniumtest/src/test/java/runners/UserAuthTest.java
@@ -0,0 +1,19 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+)
+public class UserAuthTest extends AbstractTestNGCucumberTests {
+
+}
+/*
+ * features = "src//test//resources//features//UserAuthentication.feature"
+ * 
+ * ,glue = "stepDefinations"
+ * ,plugin = {"pretty", "usage:target\\UsageReport"
+ */
+
+
+//}
