fcf676b649a825582ef7a75a06682f0a6c991e38
Administrator <Administrator@172.31.12.0>
2024-03-22 07:51:00 +0000

    tday changes

--- Seleniumtest/src/test/java/runners/GoogleSearhRunner.java
+++ Seleniumtest/src/test/java/runners/GoogleSearhRunner.java
@@ -3,7 +3,7 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src//test//resources//Features//GoogleSearchDataTableMap.feature"
+@CucumberOptions(features = "src//test//resources//Features//GoogleSearhRunner.feature"
 ,glue = "stepDefs",
 monochrome=true,
 dryRun = false
